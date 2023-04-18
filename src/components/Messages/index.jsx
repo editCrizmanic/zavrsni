c253f103cca0d30090528899d7cd2b11c89ee27c
Edita Krizmanic <ekrizmanic01@gmail.com>
2023-04-18 21:42:25 +0200

    without repeating username

--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 
-const renderMessage = (message, currentMember) => {
+const renderMessage = (id, currentMember, message) => {
     const {member, text} = message;
     const messageFromMe = member.username === currentMember.username;
     const className = messageFromMe ? "Messages-message currentMember" : "Messages-message";
 
+    // check if the current member is the same as the previous member
+    if (renderMessage.prevMember && renderMessage.prevMember.username === member.username) {
+        return (
+            <li key={id} className={className}>
+                <div className="Message-content">
+                    <div className="text">{text}</div>
+                </div>    
+            </li>
+        );
+    }
+
+    // display the member details for a new member
+    renderMessage.prevMember = member;
     return (
-        <li className={className}>
+        <li key={id} className={className}>
             <span
                 className="avatar"
                 style={{backgroundColor: member.color}}
@@ -25,9 +38,11 @@ const Messages = (props) => {
     const {messages, currentMember} = props;
     return(
         <ul className="Messages-list">
-            {messages.map(m => renderMessage(m, currentMember))}
+            {messages.map((m, index) => renderMessage(index, currentMember, m))}
         </ul>
     );
 };
+
+renderMessage.prevMember = null;
 
 export default Messages;
