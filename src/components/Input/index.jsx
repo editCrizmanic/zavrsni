3f15da366695411b2c00d657b8a578d4cd2700ea
Edita Krizmanic <ekrizmanic01@gmail.com>
2023-04-29 23:18:14 +0200

    added emoji picker (console shows weird errors)

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
+import EmojiPicker, {
+  EmojiStyle,
+  SkinTones,
+  Theme,
+  Categories,
+  EmojiClickData,
+  Emoji,
+  SuggestionMode,
+  SkinTonePickerLocation
+} from "emoji-picker-react";
 
 const Input = ({ onSendMessage }) => {
   const [text, setText] = useState("");
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false)
 
   const onChange = (e) => {
     setText(e.target.value);
+  };
+
+  const togglePicker = () => {
+    setShowEmojiPicker(!showEmojiPicker);
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
     onSendMessage(text);
     setText("");
+    console.log("Submitted text:", text);
   };
-
+  
+  const onSelectEmoji = (emojiData) => {
+    setText((text) => text + emojiData.emoji);
+    console.log("Selected emoji:", emojiData);
+  };
+  
   return (
     <div className="Input">
       <form onSubmit={onSubmit}>
@@ -23,6 +44,12 @@ const Input = ({ onSendMessage }) => {
           placeholder="Type here and press ENTER"
           autoFocus={true}
         />
+        <button type="button" onClick={togglePicker}>😊</button>
+        {showEmojiPicker && (
+          <EmojiPicker
+            onEmojiClick={onSelectEmoji}
+            />
+        )}
         <button>Send</button>
       </form>
     </div>
